c76e86eaf5310721cab77145427465ade803d677
TaniaAsh <TaniaAsh@users.noreply.github.com>
2018-05-26 21:02:57 +0100

    Second commit

---
+++ first.ps1
@@ -0,0 +1,26 @@
+﻿$PSVersionTable.PSVersion
+Get-AzureSubscription
+Import-Module PowerShellGet
+Install-Module PowerShellGet -Force
+
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
+Get-Module -Name PowerShellGet -ListAvailable | Select-Object -Property Name,Version,Path
+
+# Install the Azure Resource Manager modules from the PowerShell Gallery
+Install-Module -Name AzureRM -AllowClobber -Force
+Install-Module -Name AzureRM  -Force
+
+Get-ExecutionPolicy
+Get-ExecutionPolicy -List
+Save-Module -Name PowerShellGet -Path 'C:\install\ps'
+
+
+$env:ProgramFiles\WindowsPowerShell\Modules\PowerShellGet\
+$env:ProgramFiles\WindowsPowerShell\Modules\PackageManagement\
+
+Connect-AzureRmAccount
+Get-AzureRmSubscription
+get-azurermvm
+Get-AzureRMResourceGroup | ft
+
---
+++ infrastructure.ps1
@@ -0,0 +1,26 @@
+﻿#========================================================================
+
+Connect-AzureRmAccount
+Get-AzureRmSubscription
+
+#========================================================================
+
+$rgname = "datalakeproj"
+$location = "uksouth"
+$projecttag = "datalakeproj"
+
+#========================================================================
+
+New-AzureRmResourceGroup `
+   -Name $rgname `
+   -Location $location `
+   -Tag @{Project=$projecttag}
+
+#Get-AzureRMResourceGroup | ft
+#where {$_.Tags.Name -contains "AutoShutdownSchedule"}
+
+Get-AzureRmResourceGroup | where ResourceGroupName -eq $rgname
+
+#========================================================================
+
+Remove-AzureRmResourceGroup -Name $rgname
